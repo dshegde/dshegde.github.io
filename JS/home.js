90ba14276087bc267449a1988fe0b19d9f323999
Deepa Hegde <hegde@pdx.edu>
2022-12-08 18:02:09 -0800

    added contact form implementation

--- JS/home.js
+++ JS/home.js
@@ -1,87 +1,17 @@
 
-const formDiv = document.getElementById('contact');
-const main = document.createElement('main');
-main.classList.add('container-fluid', 'mt-4', 'mb-5');
-formDiv.append(main);
-const form = document.createElement('form');
-form.classList.add('bg-dark', 'border', 'rounded', 'mx-auto', 'p-3', 'border', 'border-warning');
-main.append(form);
-
-// Start form 
-const h3 = document.createElement('h3');
-h3.classList.add('text-uppercase', 'mb-3', 'text-light');
-h3.textContent = 'Get In Touch!';
-h3.style.textAlign = 'center';
-form.append(h3);
-const nameDiv = document.createElement('div');
-nameDiv.classList.add('form-group', 'row', 'mb-3');
-form.append(nameDiv);
-const nameLabel = document.createElement('label');
-nameLabel.classList.add('col-4', 'col-form-label', 'text-light');
-nameLabel.textContent = 'Name:';
-nameLabel.style.fontSize = '20px';
-nameLabel.htmlFor = 'username';
-const username = document.createElement('input');
-username.classList.add('border', 'border-grey', 'rounded', 'col-md-7', 'col-sm-11', 'mx-sm-2', 'mx-md-2', 'col-xs-15', 'mx-xs-2');
-username.name = 'name';
-username.id = 'username';
-nameDiv.appendChild(nameLabel);
-nameDiv.appendChild(username);
-
-
-const emailDiv = document.createElement('div');
-emailDiv.classList.add('form-group', 'row', 'mb-3');
-form.append(emailDiv);
-const emailLabel = document.createElement('label');
-emailLabel.classList.add('col-4', 'col-form-label', 'text-light');
-emailLabel.textContent = 'Email:';
-emailLabel.style.fontSize = '20px';
-emailLabel.htmlFor = 'email';
-const email = document.createElement('input');
-email.classList.add('border', 'border-grey', 'rounded', 'col-md-7', 'col-sm-11', 'mx-sm-2', 'mx-md-2', 'col-xs-15', 'mx-xs-2');
-email.id = 'email';
-email.type = 'email';
-emailDiv.appendChild(emailLabel);
-emailDiv.appendChild(email);
-
-const msgDiv = document.createElement('div');
-msgDiv.classList.add('form-group', 'row', 'mb-3');
-form.append(msgDiv);
-const msgLabel = document.createElement('label');
-msgLabel.classList.add('col-4', 'col-form-label', 'text-light');
-msgLabel.textContent = 'Message:';
-msgLabel.style.fontSize = '20px';
-msgLabel.htmlFor = 'message';
-const msg = document.createElement('textarea');
-msg.classList.add('border', 'border-grey', 'rounded', 'col-md-7', 'col-sm-11', 'mx-sm-2', 'mx-md-2', 'col-xs-15', 'mx-xs-2');
-msg.type = 'text';
-msg.rows = '7';
-msg.name = 'message';
-msg.id = 'message';
-msg.placeholder = 'Leave me a message!';
-msgDiv.appendChild(msgLabel);
-msgDiv.appendChild(msg);
-
-const buttonDiv = document.createElement('div');
-buttonDiv.classList.add('form-group', 'row');
-form.append(buttonDiv);
-const submit = document.createElement('input');
-submit.id = 'submit';
-submit.type = 'submit';
-submit.value = 'Send';
-submit.classList.add('btn', 'btn-warning', 'col-5', 'mx-auto');
-const reset = document.createElement('input');
-reset.id = 'reset';
-reset.type = 'reset';
-reset.value = 'Reset';
-reset.classList.add('btn', 'btn-outline-warning', 'col-5', 'mx-auto');
-buttonDiv.appendChild(submit);
-buttonDiv.appendChild(reset);
-// End form
-
-submit.onclick = function () {
-    window.location = "mailto:hegde@pdx.edu";
-}
+// function sendEmail() {
+//   Email.send({
+//           Host : "smtp.gmail.com",
+//           Username : "deepa.hegde79@gmail.com",
+//           Password : "Hombelaku",
+//           To : 'hegdedeepa79@gmail.com',
+//           From : document.getElementById('email').value,
+//           Subject : "PORTFOLIO MESSAGE!!!",
+//           Body : document.getElementById('message').value
+//       }).then(
+//         message => alert("Sent")
+//       );
+// }
 
 let collap = document.getElementsByClassName("collapsible");
 
